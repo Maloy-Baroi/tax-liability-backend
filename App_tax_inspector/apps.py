281344df2070e2b5e67b29edc9406e05439ef923
maloy.baroi <maloy.baroi@technonext.com>
2023-07-31 21:10:15 +0600

    Initial

---
+++ App_tax_inspector/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppTaxInspectorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'App_tax_inspector'
---
+++ App_tax_payer/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppTaxPayerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'App_tax_payer'
