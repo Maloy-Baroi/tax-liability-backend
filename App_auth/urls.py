e1cff524a029be0455d76ffd524467b77a4c6862
maloy.baroi <maloy.baroi@technonext.com>
2023-08-02 23:32:39 +0600

    Sending tax payer bool result

--- App_auth/urls.py
+++ App_auth/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import CustomUserListCreateView, CustomUserRetrieveUpdateDeleteView
+from .views import CustomUserListCreateView, CustomUserRetrieveUpdateDeleteView, CustomTokenObtainPairView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('users/', CustomUserListCreateView.as_view(), name='user-list-create'),
     path('users/<int:pk>/', CustomUserRetrieveUpdateDeleteView.as_view(), name='user-retrieve-update-delete'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- App_auth/views.py
+++ App_auth/views.py
@@ -1,6 +1,11 @@
 from rest_framework import generics
+
+from App_tax_payer.models import TaxPayer
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 # Create your views here.
@@ -12,3 +17,21 @@ class CustomUserListCreateView(generics.ListCreateAPIView):
 class CustomUserRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        if response.status_code == status.HTTP_200_OK:
+            try:
+                user = request.user
+                tax_payer = TaxPayer.objects.get(user=user.id)
+                is_tax_payer = True
+            except TaxPayer.DoesNotExist:
+                is_tax_payer = False
+
+            data = response.data
+            data['is_tax_payer'] = is_tax_payer
+            return Response(data)
+
+        return response
